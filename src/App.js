c23ffd77f0054638c9f514f24bd3c7c26512e429
Marinkie <69327534+Marinkiet@users.noreply.github.com>
2022-01-21 00:51:20 +0200

    changed search box size 50%,search filters by location(city)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import { Component } from 'react';
 import { CardList } from './components/card-list/card-list-component';
+import {SearchBox} from './components/Searchbox/search-box.component'
+
 class App extends Component {
  
   constructor(){
     super()
     this.state = {
-      monstors:[]
+      monstors:[],
+      searchField:''
       
     };
   }
@@ -17,12 +20,18 @@ class App extends Component {
     .then(users => this.setState({monstors:users}));
   }
   render(){
+    const {monstors, searchField} = this.state;
+    const filteredMonstors = monstors.filter(monstor =>
+      monstor.address.city.toLowerCase().includes(searchField.toLowerCase()));
     return(
       <div className="App">
-        <CardList monstors={this.state.monstors} >
-       
-        </CardList>
-        
+       <h1>Hustlers</h1>
+        <SearchBox 
+        placeholder= 'Search hustlers Location'
+        handelchange = {e =>this.setState({searchField: e.target.value})}
+         />
+         <CardList monstors={filteredMonstors}/>
+
       </div>
     )
   }
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,10 +3,9 @@ import React from 'react';
 import './card.styles.css'
 export const Card = props=>(
     <div className ='card-container'>
-        <img alt="monstor" src={`https://robohash.org/${props.monstor.id}?set=set2&size=180x180`}></img>
+        <img alt="monstor" src={`https://robohash.org/${props.monstor.id}?set=set2&size=140x140`}></img>
         <h2 >{props.monstor.name}</h2>
-        <p>{props.monstor.email}</p>
         <p>{props.monstor.address.city}</p>
-        <p>{props.monstor.address.street}</p>
+       
     </div>
 )
